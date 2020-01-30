bdc3ad25500a43fe7b756fb7451ae4af59b4a124
Lukas Schmierer <lukas.schmierer@lschmierer.de>
2020-01-30 17:58:32 +0100

    check ios

--- ios/Classes/SwiftAndroidPlayInstallReferrerPlugin.swift
+++ ios/Classes/SwiftAndroidPlayInstallReferrerPlugin.swift
@@ -3,12 +3,12 @@ import UIKit
 
 public class SwiftAndroidPlayInstallReferrerPlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "android_play_install_referrer", binaryMessenger: registrar.messenger())
+    let channel = FlutterMethodChannel(name: "de.lschmierer.android_play_install_referrer", binaryMessenger: registrar.messenger())
     let instance = SwiftAndroidPlayInstallReferrerPlugin()
     registrar.addMethodCallDelegate(instance, channel: channel)
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
+    result(FlutterMethodNotImplemented)
   }
 }
