8b6c834fd2c0324c00a46892293711b865a594a1
Siarhei Alikhver <solikhver@users.noreply.github.com>
2021-12-15 15:26:14 +0100

    Refactoring to conform thread safety

--- android/src/main/kotlin/de/lschmierer/android_play_install_referrer/AndroidPlayInstallReferrerPlugin.kt
+++ android/src/main/kotlin/de/lschmierer/android_play_install_referrer/AndroidPlayInstallReferrerPlugin.kt
@@ -5,92 +5,94 @@ import androidx.annotation.NonNull
 import com.android.installreferrer.api.InstallReferrerClient
 import com.android.installreferrer.api.InstallReferrerStateListener
 import io.flutter.embedding.engine.plugins.FlutterPlugin
-import io.flutter.plugin.common.BinaryMessenger
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
+import kotlinx.coroutines.*
 
 
+@ExperimentalCoroutinesApi
 /** AndroidPlayInstallReferrerPlugin */
-class AndroidPlayInstallReferrerPlugin : FlutterPlugin, MethodCallHandler, InstallReferrerStateListener {
-    private val CHANNEL_NAME = "de.lschmierer.android_play_install_referrer"
+class AndroidPlayInstallReferrerPlugin : FlutterPlugin, MethodCallHandler {
+
+    companion object {
+        const val CHANNEL_NAME = "de.lschmierer.android_play_install_referrer"
+    }
 
     private lateinit var context: Context
     private var channel: MethodChannel? = null
-    private var referrerClient: InstallReferrerClient? = null;
-    private var currentResult: Result? = null;
 
     override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-        setupChannel(flutterPluginBinding.binaryMessenger, flutterPluginBinding.applicationContext)
-    }
-
-    private fun setupChannel(messenger: BinaryMessenger, context: Context) {
-        this.context = context
-        channel = MethodChannel(messenger, CHANNEL_NAME)
+        this.context = flutterPluginBinding.applicationContext
+        channel = MethodChannel(flutterPluginBinding.binaryMessenger, CHANNEL_NAME)
         channel!!.setMethodCallHandler(this)
-    }
-
-    override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
-        if (call.method == "getInstallReferrer") {
-            getInstallReferrer(result)
-        } else {
-            result.notImplemented()
-        }
-    }
-
-    private fun getInstallReferrer(@NonNull result: Result) {
-        currentResult = result;
-        referrerClient = InstallReferrerClient.newBuilder(context).build()
-        referrerClient!!.startConnection(this)
     }
 
     override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
         channel?.setMethodCallHandler(null)
         channel = null
-        referrerClient = null;
-        currentResult = null;
     }
 
-    override fun onInstallReferrerSetupFinished(responseCode: Int) {
-        when (responseCode) {
-            InstallReferrerClient.InstallReferrerResponse.OK -> {
-                val referrerDetails = referrerClient?.installReferrer
-
-                if (referrerDetails != null) {
-                    val details = HashMap<String, Any?>()
-                    details["installReferrer"] = referrerDetails.installReferrer
-                    details["referrerClickTimestampSeconds"] = referrerDetails.referrerClickTimestampSeconds
-                    details["installBeginTimestampSeconds"] = referrerDetails.installBeginTimestampSeconds
-                    details["referrerClickTimestampServerSeconds"] = referrerDetails.referrerClickTimestampServerSeconds
-                    details["installBeginTimestampServerSeconds"] = referrerDetails.installBeginTimestampServerSeconds
-                    details["installVersion"] = referrerDetails.installVersion
-                    details["googlePlayInstantParam"] = referrerDetails.googlePlayInstantParam
-
-                    currentResult?.success(details)
-                } else {
-                    currentResult?.error("BAD_STATE", "Result is null.", null);
+    override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
+        if (call.method == "getInstallReferrer") {
+            val client = InstallReferrerClient.newBuilder(context).build()
+            runBlocking {
+                val res = withContext(Dispatchers.Default) { awaitCallback(client) }
+                when (res.first) {
+                    InstallReferrerClient.InstallReferrerResponse.OK -> {
+                        if (res.second.isNotEmpty()) {
+                            result.success(res.second)
+                        } else {
+                            result.error("BAD_STATE", "Result is null.", null)
+                        }
+                    }
+                    InstallReferrerClient.InstallReferrerResponse.FEATURE_NOT_SUPPORTED -> {
+                        result.error("FEATURE_NOT_SUPPORTED", "API not available on the current Play Store app.", null)
+                    }
+                    InstallReferrerClient.InstallReferrerResponse.SERVICE_UNAVAILABLE -> {
+                        result.error("SERVICE_UNAVAILABLE", "Connection couldn't be established.", null)
+                    }
+                    InstallReferrerClient.InstallReferrerResponse.PERMISSION_ERROR -> {
+                        result.error("PERMISSION_ERROR", "App is not allowed to bind to the Service.", null)
+                    }
                 }
             }
-            InstallReferrerClient.InstallReferrerResponse.FEATURE_NOT_SUPPORTED -> {
-                currentResult?.error("FEATURE_NOT_SUPPORTED", "API not available on the current Play Store app.", null)
-                return
-            }
-            InstallReferrerClient.InstallReferrerResponse.SERVICE_UNAVAILABLE -> {
-                currentResult?.error("SERVICE_UNAVAILABLE", "Connection couldn't be established.", null)
-                return
-            }
-            InstallReferrerClient.InstallReferrerResponse.PERMISSION_ERROR -> {
-                currentResult?.error("PERMISSION_ERROR", "App is not allowed to bind to the Service.", null)
-                return
-            }
+        } else {
+            result.notImplemented()
         }
-        referrerClient?.endConnection()
-        referrerClient = null;
     }
 
-    override fun onInstallReferrerServiceDisconnected() {
-        referrerClient = null;
-        currentResult = null;
+
+    private suspend fun awaitCallback(client: InstallReferrerClient): Pair<Int, Map<String, Any?>> = suspendCancellableCoroutine { continuation ->
+        val callback = object : InstallReferrerStateListener { // Implementation of some callback interface
+            override fun onInstallReferrerSetupFinished(responseCode: Int) {
+                val details = HashMap<String, Any?>()
+
+                if (responseCode == InstallReferrerClient.InstallReferrerResponse.OK) {
+                    val referrerDetails = client.installReferrer
+
+                    if (referrerDetails != null) {
+                        details["installReferrer"] = referrerDetails.installReferrer
+                        details["referrerClickTimestampSeconds"] = referrerDetails.referrerClickTimestampSeconds
+                        details["installBeginTimestampSeconds"] = referrerDetails.installBeginTimestampSeconds
+                        details["referrerClickTimestampServerSeconds"] = referrerDetails.referrerClickTimestampServerSeconds
+                        details["installBeginTimestampServerSeconds"] = referrerDetails.installBeginTimestampServerSeconds
+                        details["installVersion"] = referrerDetails.installVersion
+                        details["googlePlayInstantParam"] = referrerDetails.googlePlayInstantParam
+                    }
+                }
+                client.endConnection()
+                // Resume coroutine with a value provided by the callback
+                continuation.resume(Pair(responseCode, details)) { client.endConnection() }
+            }
+
+            override fun onInstallReferrerServiceDisconnected() {
+            }
+        }
+        // Register callback with an API
+        client.startConnection(callback)
+        // Remove callback on cancellation
     }
+
 }
