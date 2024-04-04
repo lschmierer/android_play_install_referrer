ad0964eb7ebdb4b261e37b7fbbab9cb31fc521e4
Josh Burton <josh@arklabs.nz>
2024-04-04 13:09:37 +1300

    updates Gradle to 8.3 and AGP to 8.1.0, java/kotlin compilation target to 1.8

--- example/test/widget_test.dart
+++ example/test/widget_test.dart
@@ -19,7 +19,7 @@ void main() {
     expect(
       find.byWidgetPredicate(
         (Widget widget) => widget is Text &&
-                           widget.data.startsWith('Referrer Details:'),
+                           widget.data!.startsWith('Referrer Details:'),
       ),
       findsOneWidget,
     );
