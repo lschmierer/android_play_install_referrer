a6fc47b0790c3a2f31620b12c84da703f4b20481
Lev Berlinkov <107831888+levBerlinkov@users.noreply.github.com>
2025-01-14 22:48:48 +0200

    added imports

--- android/src/main/kotlin/de/lschmierer/android_play_install_referrer/AndroidPlayInstallReferrerPlugin.kt
+++ android/src/main/kotlin/de/lschmierer/android_play_install_referrer/AndroidPlayInstallReferrerPlugin.kt
@@ -11,6 +11,8 @@ import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 import kotlin.collections.ArrayList
+import android.os.DeadObjectException
+import android.util.Log
 
 
 /** AndroidPlayInstallReferrerPlugin */
